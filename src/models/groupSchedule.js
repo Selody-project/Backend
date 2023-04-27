3bc55bc597dae193fc071e248c09e6564eea2181
hanyiseo2 <shyshy81818181@gmail.com>
2023-04-27 21:59:41 +0900

    edit attribute of id in GroupShedule

--- src/models/groupSchedule.js
+++ src/models/groupSchedule.js
@@ -1,62 +1,63 @@
-const Sequelize = require('sequelize')
+const Sequelize = require('sequelize');
 
 class GroupSchedule extends Sequelize.Model {
-    static initiate(sequelize) {
-        GroupSchedule.init({
-        id:{
-            type:Sequelize.BIGINT,
-            primaryKey
-        },
-        groupId:{
-            type:Sequelize.BIGINT,
-            primaryKey : true,
-            allowNull: false,
-        },
-        title:{
-            type:Sequelize.STRING(45),
-            allowNull: false,
-        },
-        content:{
-            type:Sequelize.STRING(45),
-            allowNull: true,
-        },
-        startDate:{
-            type:Sequelize.DATE,
-            allowNull:false,
-        },
-        endDate:{
-            type:Sequelize.DATE,
-            allowNull:false,
-        },
-        repeat:{
-            type:Sequelize.TINYINT,
-            allowNull:false,
-        },
-        repeatType:{
-            type:Sequelize.ENUM('year', 'month', 'week'),
-            allowNull:true,
-        },
-        confirmed:{
-            type:Sequelize.TINYINT,
-            allowNull: false,
-        },
-        possible:{
-            type:JSON,
-            allowNull:true,
-        },
-        impossible:{
-            type:JSON,
-            allowNull:true,
-        }
+  static initiate(sequelize) {
+    GroupSchedule.init({
+      id: {
+        type: Sequelize.BIGINT,
+        primaryKey: true,
+        allowNull: false,
+      },
+      groupId: {
+        type: Sequelize.BIGINT,
+        primaryKey: true,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING(45),
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING(45),
+        allowNull: true,
+      },
+      startDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      endDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      repeat: {
+        type: Sequelize.TINYINT,
+        allowNull: false,
+      },
+      repeatType: {
+        type: Sequelize.ENUM('year', 'month', 'week'),
+        allowNull: true,
+      },
+      confirmed: {
+        type: Sequelize.TINYINT,
+        allowNull: false,
+      },
+      possible: {
+        type: JSON,
+        allowNull: true,
+      },
+      impossible: {
+        type: JSON,
+        allowNull: true,
+      },
     }, {
-        sequelize,
-        timestamps: true,
-        modelName: 'GroupSchedule',
-        tableName: 'groupSchedule',
-        charset: 'utf8',
-        collate: 'utf8_general_ci',
-    })
-    }
+      sequelize,
+      timestamps: true,
+      modelName: 'GroupSchedule',
+      tableName: 'groupSchedule',
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    });
+  }
 }
 
-module.exports = GroupSchedule
+module.exports = GroupSchedule;
