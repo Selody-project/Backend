46e2f29a30af107d43be7931d115cf0e3f77cc3a
hanyiseo2 <122385460+hanyiseo2@users.noreply.github.com>
2023-04-27 22:48:57 +0900

    Issue#4,5 (#18)
    * create group table and groupSchedule table
    
    * create table
    
    * Fixing BIGINT in groupSchedule table
    
    * edit attribute of id in GroupShedule
    
    * fixing timestamps by false

---
+++ src/models/groupSchedule.js
@@ -0,0 +1,63 @@
+const Sequelize = require('sequelize');
+
+class GroupSchedule extends Sequelize.Model {
+  static initiate(sequelize) {
+    GroupSchedule.init({
+      id: {
+        type: Sequelize.BIGINT,
+        primaryKey: true,
+        allowNull: false,
+      },
+      groupId: {
+        type: Sequelize.BIGINT,
+        primaryKey: true,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING(45),
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING(45),
+        allowNull: true,
+      },
+      startDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      endDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      repeat: {
+        type: Sequelize.TINYINT,
+        allowNull: false,
+      },
+      repeatType: {
+        type: Sequelize.ENUM('year', 'month', 'week'),
+        allowNull: true,
+      },
+      confirmed: {
+        type: Sequelize.TINYINT,
+        allowNull: false,
+      },
+      possible: {
+        type: JSON,
+        allowNull: true,
+      },
+      impossible: {
+        type: JSON,
+        allowNull: true,
+      },
+    }, {
+      sequelize,
+      timestamps: false,
+      modelName: 'GroupSchedule',
+      tableName: 'groupSchedule',
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    });
+  }
+}
+
+module.exports = GroupSchedule;
