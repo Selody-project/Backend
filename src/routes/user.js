188ff9133a737c983b77bbdbe04a7b6949f9bdd0
GGANCC1 <81506668+GGANCC1@users.noreply.github.com>
2023-05-08 01:01:22 +0900

    ISSUE7-GET-user-calendar-todo 임시 커밋

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const {
   getUserSchedule,
+  getUserDaySchedule,
 } = require('../controllers/user');
-const { verifyToken } = require('../middleware/token');
 
 const router = express.Router();
 
-router.get('/:user_id/calendar', verifyToken, getUserSchedule);
+router.get('/:user_id/calendar', getUserSchedule);
+
+router.get('/:user_id/calendar/todo', getUserDaySchedule);
 
 module.exports = router;
