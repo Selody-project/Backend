aeb493d654de94c7eb2159cb79e45ecf63c4e81a
LeeSeungWoo <81506668+GGANCC1@users.noreply.github.com>
2023-05-28 00:14:39 +0900

    ISSUE68-token-custom-error (#71)

--- src/errors/auth/index.js
+++ src/errors/auth/index.js
@@ -1,7 +1,11 @@
 const InvalidPasswordError = require('./InvalidIdPasswordError');
 const DuplicateUserError = require('./DuplicateUserError');
+const InvalidTokenError = require('./InvalidTokenError');
+const TokenExpireError = require('./TokenExpireError');
 
 module.exports = {
   InvalidPasswordError,
   DuplicateUserError,
+  InvalidTokenError,
+  TokenExpireError,
 };
