89c137f81bf190ee5c09819c1edc0896b6e3651b
GGANCC1 <81506668+GGANCC1@users.noreply.github.com>
2023-07-08 17:29:02 +0900

    ISSUE114-get-group-detail

--- src/routes/group.js
+++ src/routes/group.js
@@ -3,6 +3,7 @@ const express = require('express');
 const {
   createGroup,
   getGroupList,
+  getGroupDetail,
   deleteGroup,
   patchGroup,
   getGroupSchedule,
@@ -18,12 +19,13 @@ const router = express.Router();
 
 router.get('/', getGroupList);
 router.post('/', createGroup);
-router.delete('/:id', deleteGroup);
-router.patch('/:id', patchGroup);
+router.get('/:group_id', getGroupDetail);
+router.delete('/:group_id', deleteGroup);
+router.patch('/:group_id', patchGroup);
 router.post('/calendar', postGroupSchedule);
 router.put('/calendar/:id', putGroupSchedule);
 router.delete('/calendar/:id', deleteGroupSchedule);
-router.get('/:id/calendar', getGroupSchedule);
+router.get('/:group_id/calendar', getGroupSchedule);
 router.post('/:group_id/invite-link', postInviteLink);
 router.get('/invite-link/:inviteCode', getInvitation);
 router.post('/join/:inviteCode', postGroupJoin);
