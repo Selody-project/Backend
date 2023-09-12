00a256b16db07f3663e1705082c764b7bd528a27
이승우 <81506668+GGANCC1@users.noreply.github.com>
2023-09-12 19:25:33 +0900

    ISSUE191-fix-delete-group-member (#195)
    * update test code
    
    * update swagger

--- src/errors/group/index.js
+++ src/errors/group/index.js
@@ -1,9 +1,11 @@
 const GroupNotFoundError = require('./GroupNotFoundError');
 const ExpiredCodeError = require('./ExpiredCodeError');
 const InvalidGroupJoinError = require('./InvalidGroupJoinError');
+const NoBanPermission = require('./NoBanPermission');
 
 module.exports = {
   GroupNotFoundError,
   ExpiredCodeError,
   InvalidGroupJoinError,
+  NoBanPermission,
 };
