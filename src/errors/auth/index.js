e51c0d535ce0bef9dadba90e1ec7a2dfef6668d5
GGANCC1 <81506668+GGANCC1@users.noreply.github.com>
2023-09-21 16:04:57 +0900

    ISSUE205-fix-duplicate-error

--- src/errors/auth/index.js
+++ src/errors/auth/index.js
@@ -1,11 +1,15 @@
 const InvalidIdPasswordError = require('./InvalidIdPasswordError');
 const DuplicateUserError = require('./DuplicateUserError');
+const DuplicateEmailError = require('./DuplicateEmailError');
+const DuplicateNicknameError = require('./DuplicateNicknameError');
 const InvalidTokenError = require('./InvalidTokenError');
 const TokenExpireError = require('./TokenExpireError');
 
 module.exports = {
   InvalidIdPasswordError,
   DuplicateUserError,
+  DuplicateEmailError,
+  DuplicateNicknameError,
   InvalidTokenError,
   TokenExpireError,
 };
