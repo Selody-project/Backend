1580a20d9062e64b943db0b4bae141c13ceead57
kimyoungyin <78777345+kimyoungyin@users.noreply.github.com>
2024-03-19 17:14:57 +0900

    ISSUE310 fix: change wrong environment PORT value in config file and maximize rateLimit(#310) (#311)

--- src/config/config.js
+++ src/config/config.js
@@ -1,31 +1,31 @@
 /* istanbul ignore file */
 
-const testEnv = process.env.NODE_ENV === 'test';
+const testEnv = process.env.NODE_ENV === "test";
 const dbHost = testEnv ? process.env.DB_TEST_HOST : process.env.DB_HOST;
 const dbPort = testEnv ? process.env.DB_TEST_PORT : process.env.DB_PORT;
 const dbUsername = testEnv
-  ? process.env.DB_TEST_USERNAME
-  : process.env.DB_USERNAME;
+    ? process.env.DB_TEST_USERNAME
+    : process.env.DB_USERNAME;
 const dbPassword = testEnv
-  ? process.env.DB_TEST_PASSWORD
-  : process.env.DB_PASSWORD;
+    ? process.env.DB_TEST_PASSWORD
+    : process.env.DB_PASSWORD;
 const dbName = testEnv ? process.env.DB_TEST_NAME : process.env.DB_NAME;
 
 const config = {
-  APP_URL: process.env.APP_URL,
-  PORT: process.env.PORT,
+    APP_URL: process.env.APP_URL,
+    PORT: process.env.APP_PORT,
 };
 
 const dbConfig = {
-  username: dbUsername,
-  password: dbPassword,
-  database: dbName,
-  host: dbHost,
-  dialect: 'mysql',
-  port: dbPort,
+    username: dbUsername,
+    password: dbPassword,
+    database: dbName,
+    host: dbHost,
+    dialect: "mysql",
+    port: dbPort,
 };
 
 module.exports = {
-  config,
-  dbConfig,
+    config,
+    dbConfig,
 };
